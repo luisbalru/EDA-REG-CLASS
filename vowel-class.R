08fdd081a3abb9096d8c2f7b8237e813e8387d11
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-12-03 13:55:27 +0100

    estudiando la estructura del vowel #4

--- vowel-class.R
+++ vowel-class.R
@@ -9,3 +9,6 @@
 # Lectura del fichero de datos para proceder al EDA
 vowel = read.csv("./data/vowel/vowel.dat",header=FALSE, comment.char="@")
 colnames(vowel) = c("TT","SpeakerNumber","Sex","F0","F1","F2","F3","F4","F5","F6","F7","F8","F9","Class")
+
+# Estudiemos la estructura del conjunto
+str(vowel)
