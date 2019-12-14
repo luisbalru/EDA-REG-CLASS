cefc5726fb1f9e85a8ed44542ab51c612eb95f10
luisbalru <balderasruizluis@gmail.com>
2019-12-14 17:02:29 +0100

    Leído wankara #5 #7

--- wankara-reg.R
+++ wankara-reg.R
@@ -4,3 +4,6 @@
 # EDA+Regresion
 # Dataset: wankara
 ############################################################
+
+wankara = read.csv("./data/wankara/wankara.dat",header=FALSE, comment.char = "@")
+colnames(wankara) = c("Max_temperature", "Min_temperature", "Dewpoint", "Precipitation", "Sea_level_pressure", "Standard_pressure", "Visibility", "Wind_speed", "Max_wind_speed","Mean_temperature")
