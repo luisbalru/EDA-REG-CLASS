cd15430298f4684dc8cef95e2af34d5f5ad48110
luisbalru <balderasruizluis@gmail.com>
2019-12-14 17:39:52 +0100

    viendo histogramas #5

--- wankara-reg.R
+++ wankara-reg.R
@@ -7,3 +7,25 @@
 
 wankara = read.csv("./data/wankara/wankara.dat",header=FALSE, comment.char = "@")
 colnames(wankara) = c("Max_temperature", "Min_temperature", "Dewpoint", "Precipitation", "Sea_level_pressure", "Standard_pressure", "Visibility", "Wind_speed", "Max_wind_speed","Mean_temperature")
+
+
+# Resumen estadístico
+summary(wankara)
+
+# Visualización de las variables respecto de mean_temperature
+temp <- wankara
+plotY <- function (x,y) {
+  plot(temp[,y]~temp[,x], xlab=paste(names(temp)[x]),
+       ylab=names(temp)[y])
+}
+par(mfrow=c(3,4)) #Si margin too large => (2,3)
+x <- sapply(1:(dim(temp)[2]-1), plotY, dim(temp)[2])
+par(mfrow=c(1,1))
+
+library("scales")
+wankara_scale = sapply(wankara,rescale)
+wankara_scale = as.data.frame(wankara_scale)
+summary(wankara_scale)
+
+
+
