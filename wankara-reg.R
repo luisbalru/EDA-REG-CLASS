519f09669b8574f1f37c6bd2f7e85b54a76974fd
luisbalru <balderasruizluis@gmail.com>
2019-12-14 16:57:52 +0100

    comenzando regresión #5 #7

---
+++ wankara-reg.R
@@ -0,0 +1,6 @@
+###########################################################
+# INTRODUCCIÓN A LA CIENCIA DE DATOS
+# Autor: Luis Balderas Ruiz
+# EDA+Regresion
+# Dataset: wankara
+############################################################
